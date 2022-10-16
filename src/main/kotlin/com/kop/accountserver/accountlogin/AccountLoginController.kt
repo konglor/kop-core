d2ec8d3b7881c994e6d9630262465e4bb4b4a884
monkeylord <tech.monkeylor@gmail.com>
2022-10-16 16:07:00 -0500

    cleanup

--- src/main/kotlin/com/kop/accountserver/accountlogin/AccountLoginController.kt
+++ src/main/kotlin/com/kop/accountserver/accountlogin/AccountLoginController.kt
@@ -5,10 +5,6 @@ import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.PathVariable
 import org.slf4j.LoggerFactory
-import java.util.*
-
-val <T> Optional<T>.value: T?
-  get() = orElse(null)
 
 @Controller("api/accountlogin")
 class AccountLoginController(private val accountLoginService: AccountLoginService) {
@@ -16,7 +12,7 @@ class AccountLoginController(private val accountLoginService: AccountLoginServic
 
   @Get("/{id}")
   fun find(@PathVariable id: Int): HttpResponse<String?> {
-    val accountLogin: AccountLogin? = accountLoginService.findById(id).value
+    val accountLogin: AccountLogin? = accountLoginService.findById(id).get()
     val name: String? = accountLogin?.name
     return HttpResponse.ok(name)
   }
