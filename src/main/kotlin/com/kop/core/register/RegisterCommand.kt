935cf4aa3f76b113644433aa39e3170a2f00a553
monkeylord <tech.monkeylor@gmail.com>
2022-10-18 21:27:45 -0500

    sonar

--- src/main/kotlin/com/kop/core/register/RegisterCommand.kt
+++ src/main/kotlin/com/kop/core/register/RegisterCommand.kt
@@ -6,7 +6,7 @@ import javax.validation.constraints.Pattern
 import javax.validation.constraints.Size
 
 @Introspected
-open class  RegisterCommand {
+open class RegisterCommand {
   @NotBlank
   @Size(min = 5, max = 15)
   @Pattern(regexp = "^[a-zA-Z0-9]+\$", message = "username may only contain alphanumerics")
